eb4457318506f51cbf3a6ddb5c546b332cfdf5a8
Mattool81974 <mmattmmo@gmail.com>
2023-01-04 14:36:56 +0100

    Optimise defi 3

--- Projet.py
+++ Projet.py
@@ -18,38 +18,57 @@ def binStrToInt(n: str): #Convertir un int binaire dans un str en entier base di
             resultat += 2 ** i[0]
     return resultat
 
-def defi3(imageLien: str, n: int): #Fonction correspondant au défi 3
+def defi3(imageLien: str, nw: int, nh: int = None): #Fonction correspondant au défi 3
+    if nh is None:
+        nh = nw
     image = open(imageLien) #Chargé l'image
     imageTaille = image.size #Obtenir la taille de l'image
-    imageTailleErreurY = imageTaille[1]%n
-    offsetX = 0
-    offsetY = 0
-    for y in range(0, imageTaille[1] - n, n):
-        imageTailleErreurX = imageTaille[0]%n
-        y += offsetY
-        offsetErreurY = 0
-        if imageTailleErreurY > 0:
-            offsetErreurY = 1
-            imageTailleErreurY -= offsetErreurY
-        for x in range(0, imageTaille[0] - n, n): #Parcourir chaque pixel de l'image
+    imagePixeliseTaille = (imageTaille[0]//nw, imageTaille[1]//nh) #Obtenir la taille de l'image en pixel de n de large
+    imageTailleErreurY = imageTaille[1]%nh #Nombre de pixels d'erreurs à la hauteur
+    IMAGETAILLEERREURY = imageTaille[1]%nh #Pareil que ImageTailleErreurY mais constant
+    imageTailleErreurYErreur = IMAGETAILLEERREURY%imagePixeliseTaille[1] #Nombre de pixel d'erreurs pour le calcul du nombre de pixels d'erreurs à la hauteur
+    offsetY = 0 #Taux de décalage de y pour éviter des erreurs et des zones non pixellés
+    multiplieurHauteur = 1 #Variable permettant de compter ou non la hauteur à retirer
+    if imageTailleErreurY == 0: #Si il n'y aura aps de pixels non pixellisés
+        multiplieurHauteur = 0 #Ignorer la hauteur à retirer
+    for y in range(0, imageTaille[1] - nh * multiplieurHauteur, nh): #Parcourir chaque pixel de l'image
+        imageTailleErreurX = imageTaille[0]%nw #Nombre de pixels d'erreurs à la largeur
+        IMAGETAILLEERREURX = imageTaille[0] % nw  # Pareil que ImageTailleErreurY mais constant
+        imageTailleErreurXErreur = IMAGETAILLEERREURX%imagePixeliseTaille[0] ##Nombre de pixel d'erreurs pour le calcul du nombre de pixels d'erreurs à la largeur
+        y += offsetY #Décaler y selon le taux de décalage à appliquer
+        offsetErreurY = 0 #Décalage à appliquer pour que le pixel qui va être pixellisé soit adapté
+        if imageTailleErreurY > 0: #Si un décalage doit être appliqué
+            offsetErreurY = floor(IMAGETAILLEERREURY/imagePixeliseTaille[1]) #Générer le décalage à appliquer pour que le pixel qui va être pixellisé soit adapté
+            if imageTailleErreurYErreur > 0: #Si le nombre de pixels d'erreurs à la hauteur à une erreur
+                offsetErreurY += 1 #Décaler encore le décalage de y pour éviter cette erreur
+                imageTailleErreurYErreur -= 1 #Modification du décalage totale en prenant en compte que celui ci est fait
+            imageTailleErreurY -= offsetErreurY #Modification du décalage totale en prenant en compte que celui ci est fait
+        offsetX = 0 #Taux de décalage de x pour éviter des erreurs et des zones non pixellés
+        multiplieurLargeur = 1  # Variable permettant de compter ou non la largeur à retirer
+        if imageTailleErreurX == 0:  # Si il n'y aura pas de pixels non pixellisés
+            multiplieurLargeur = 0  # Ignorer la  largeur à retirer
+        for x in range(0, imageTaille[0] - nw * multiplieurLargeur, nw):
             x += offsetX
             offsetErreurX = 0
             if imageTailleErreurX > 0:
-                offsetErreurX = 1
+                offsetErreurX = floor(IMAGETAILLEERREURX/imagePixeliseTaille[0])
+                if imageTailleErreurXErreur > 0:  # Si le nombre de pixels d'erreurs à la largeur à une erreur
+                    offsetErreurX += 1  # Décaler encore le décalage de y pour éviter cette erreur
+                    imageTailleErreurXErreur -= 1  # Modification du décalage totale en prenant en compte que celui ci est fait
                 imageTailleErreurX -= offsetErreurX
             somme = [0, 0, 0] #Somme des valeurs des couleurs de l'image
             nb = 0 #Nombre de pixels traités
-            for i in range(n + offsetErreurY):
-                for j in range(n + offsetErreurX): #Calcul du pixel agrandi
-                    if y > 99 and i > 98: print(y, i, y+i, x, j, x+j, (x + j, y + i), image.size)
+            for i in range(nh + offsetErreurY):
+                for j in range(nw + offsetErreurX): #Calcul du pixel agrandi
+                    #print(y, i, y+i, x, j, x+j, (x + j, y + i), image.size)
                     couleur = image.getpixel((x + j, y + i))
                     somme[0] += couleur[0] #Calcul de la somme
                     somme[1] += couleur[1]
                     somme[2] += couleur[2]
                     nb += 1
             couleurFinal = (round(somme[0] / nb), round(somme[1] / nb), round(somme[2] / nb)) #Calcul de la moyenne
-            for i in range(n + offsetErreurY):
-                for j in range(n + offsetErreurX): #Calcul du pixel agrandi
+            for i in range(nh + offsetErreurY):
+                for j in range(nw + offsetErreurX): #Calcul du pixel agrandi
                     couleur = image.putpixel((x + j, y + i), couleurFinal)
             offsetX += offsetErreurX
         offsetY += offsetErreurY
@@ -97,7 +116,7 @@ def defi5Trouver(imageLien: str):
     image.save("C:/Users/Matt_o/Pictures/A SUPPRIMER/resultat2.png")
     image.show()
 
-defi3("C:/Users/Mattéo Menou/Pictures/Donald trump.png", 99)
+defi3("C:/Users/Matt_o/Pictures/bubsy 3d.png", 57)
 #defi5Cacher("C:/Users/Matt_o/Pictures/A SUPPRIMER/test1.png", "C:/Users/Matt_o/Pictures/A SUPPRIMER/test2.png")
 #print("---------------")
 #defi5Trouver("C:/Users/Matt_o/Pictures/A SUPPRIMER/resultat.png")
